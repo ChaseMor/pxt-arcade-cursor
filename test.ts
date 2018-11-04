01b7cc979fe3d071c0aa023d978cfee1776cf9fb
ChaseMor <C_Mortensen@live.cm>
2018-11-04 00:34:48 -0700

    Added some initial designs for cursors

--- test.ts
+++ test.ts
@@ -1,1 +1,125 @@
-// tests go here; this will not be compiled when this package is used as a library
+enum SpriteKind {
+    Player,
+    Enemy
+}
+let cursor: Sprite = null
+cursor = sprites.create(img`
+f f f f f f f f f . . . . . . . 
+f 1 1 1 1 1 1 1 f . . . . . . . 
+f 1 1 1 1 1 1 f . . . . . . . . 
+f 1 1 1 1 1 f . . . . . . . . . 
+f 1 1 1 1 f 1 f . . . . . . . . 
+f 1 1 1 f 1 1 1 f . . . . . . . 
+f 1 1 f 1 1 1 1 1 f . . . . . . 
+f 1 f . f 1 1 1 1 1 f . . . . . 
+f f . . . f 1 1 1 1 1 f . . . . 
+. . . . . . f 1 1 1 1 1 f . . . 
+. . . . . . . f 1 1 1 1 1 f . . 
+. . . . . . . . f 1 1 1 f . . . 
+. . . . . . . . . f 1 f . . . . 
+. . . . . . . . . . f . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, SpriteKind.Player)
+cursor.setImage(img`
+. . . . . . f f f . . . . . . . 
+. . . . . . f 1 f . . . . . . . 
+. . . . . . f 1 f . . . . . . . 
+. . . . . . f 1 f . . . . . . . 
+. . . . . . f 1 f . . . . . . . 
+. . . . . . f 1 f . . . . . . . 
+f f f f f f f 1 f f f f f f f . 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 f . 
+f f f f f f f 1 f f f f f f f . 
+. . . . . . f 1 f . . . . . . . 
+. . . . . . f 1 f . . . . . . . 
+. . . . . . f 1 f . . . . . . . 
+. . . . . . f 1 f . . . . . . . 
+. . . . . . f 1 f . . . . . . . 
+. . . . . . f f f . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`)
+cursor.setImage(img`
+. f . . . . . . . . . . . f . . 
+f 1 f . . . . . . . . . f 1 f . 
+. f 1 f . . . . . . . f 1 f . . 
+. . f 1 f . . . . . f 1 f . . . 
+. . . f 1 f . . . f 1 f . . . . 
+. . . . f 1 f . f 1 f . . . . . 
+. . . . . f 1 f 1 f . . . . . . 
+. . . . . . f 1 f . . . . . . . 
+. . . . . f 1 f 1 f . . . . . . 
+. . . . f 1 f . f 1 f . . . . . 
+. . . f 1 f . . . f 1 f . . . . 
+. . f 1 f . . . . . f 1 f . . . 
+. f 1 f . . . . . . . f 1 f . . 
+f 1 f . . . . . . . . . f 1 f . 
+. f . . . . . . . . . . . f . . 
+. . . . . . . . . . . . . . . . 
+`)
+cursor.setImage(img`
+f f f f f f . . . . f f f f f f 
+f f f f . . . . . . . . f f f f 
+f f . . . . . . . . . . . . f f 
+f f . . . . . . . . . . . . f f 
+f . . . . . . . . . . . . . . f 
+f . . . . . . . . . . . . . . f 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+f . . . . . . . . . . . . . . f 
+f . . . . . . . . . . . . . f f 
+f f . . . . . . . . . . . . f f 
+f f . . . . . . . . . . . . f f 
+f f f f . . . . . . . . f f f f 
+f f f f f f . . . . f f f f f f 
+`)
+cursor.setImage(img`
+f f f f f f f f f . . . . . . . 
+f 1 1 1 1 1 1 1 f . . . . . . . 
+f 1 1 1 1 1 1 f . . . . . . . . 
+f 1 1 1 1 1 f . . . . . . . . . 
+f 1 1 1 1 f 1 f . . . . . . . . 
+f 1 1 1 f 1 1 1 f . . . . . . . 
+f 1 1 f 1 1 1 1 1 f . . . . . . 
+f 1 f . f 1 1 1 1 1 f . . . . . 
+f f . . . f 1 1 1 1 1 f . . . . 
+. . . . . . f 1 1 1 1 1 f . . . 
+. . . . . . . f 1 1 1 1 1 f . . 
+. . . . . . . . f 1 1 1 f . . . 
+. . . . . . . . . f 1 f . . . . 
+. . . . . . . . . . f . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`)
+cursor.setImage(img`
+. . . . . . . f . . . . . . . . 
+. . . . . . f 1 f . . . . . . . 
+. . . . . . f 1 f . . . . . . . 
+. . . . . f 1 1 1 f . . . . . . 
+. . . . . f 1 1 1 f . . . . . . 
+. . . . f 1 1 1 1 1 f . . . . . 
+. . . . f 1 1 1 1 1 f . . . . . 
+. . . f 1 1 1 1 1 1 1 f . . . . 
+. . . f 1 1 1 1 1 1 1 f . . . . 
+. . f 1 1 1 1 f 1 1 1 1 f . . . 
+. . f 1 1 1 f . f 1 1 1 f . . . 
+. . . f 1 1 f . f 1 1 f . . . . 
+. . . . f f . . . f f . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`)
+controller.controlSprite(cursor, 100, 100)
+scene.setTileMap(img`
+b b b b b b b b b b 
+b b b b b b b b b b 
+b b b b b b b b b b 
+b b b b b b b b b b 
+b b b b b b b b b b 
+b b b b b b b b b b 
+b b b b b b b b b b 
+b b b b b b b b b b 
+`)
+cursor.setFlag(SpriteFlag.Ghost, true)
